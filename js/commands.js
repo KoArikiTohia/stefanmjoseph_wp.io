1b0b14c1e3c5e70fd79fbab640c9923a0c4f2370
Stefan Joseph <stefan.joseph@lethbridgecollege.ca>
2023-11-25 22:42:35 -0700

    Initial commit

---
+++ js/commands.js
@@ -0,0 +1,105 @@
+let linkedin = 'https://www.linkedin.com/in/stefanmjoseph/';
+let instagram ='https://www.instagram.com/stefanmjoseph/';
+let tryhackme= 'https://tryhackme.com/p/KoArikiTohia';
+let existCommands = [
+  'about',
+  'banner',
+  'certifications',
+  'clear',
+  'cls',
+  'date',
+  'help',
+  'linkedin',
+  'social',
+  'theme',
+  'theme ls',
+  'theme set',
+  'tryhackme',
+  'whoami',
+  'cd ..'
+];
+
+about = [
+  "&nbsp;",
+  "&nbsp;",
+  "<span style='color:red;'>01001000 01101001</span> I'm <span style='color: yellow;'>STEFAN JOSEPH </span> ",
+  "&nbsp;",
+  "I'm a BYU-I Business graduate who is passionate about Cyber Security and connecting",   
+  "with people. I'm currently finishing my CIT Diploma at Lethbridge College.",
+  "Problems are just Opportunities that haven't presented themselves - Wilson Fisk", 
+  "&nbsp;",
+  "&nbsp;",
+  'Connect With me 💫',
+'&nbsp;',
+// '<p align="center">',
+
+//'<a href="" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="sujay" height="30" width="40" /></a> <a href="https://wa.me/918762406259?text=01101000%2001101001%20%20%F0%9F%91%8B%2C%20SUJAY%20I%20got%20your%20number%20from%20your%20site%20.%20can%20we%20have%20a%20small%20conversation%20now%20%3F%20" target="blank"><img align="center" src="https://user-images.githubusercontent.com/95465072/199298950-3882322d-7527-47d0-ac6b-b81a0ed1a1aa.png" alt="sujay_adkesar" height="45" width="50" /></a>&nbsp;&nbsp;<a href="https://www.linkedin.com/in/sujay-adkesar-a90725224" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="utkar5hm" height="30" width="40" /></a> <a href="https://instagram.com/SUJAY_ADKESAR" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="sujay" height="30" width="40" /></a>',
+
+
+'&nbsp;',
+'&nbsp;',
+];
+
+social = [
+  'TO open the below social profiles Type the desired one!',
+  'Example : linkedin',
+  '----------------',
+  '&nbsp;',
+  '<a href="https://www.linkedin.com/in/stefanmjoseph/" style="color: skyblue;">Linkedin</a>',
+  '<a href="https://www.instagram.com/stefanmjoseph/" style="color: skyblue;">Instagram</a>',
+  '<a href="https://tryhackme.com/p/KoArikiTohia/" style="color: skyblue;">TryHackMe</a>'
+];
+
+
+themes = ['dark', 'light'];
+
+theme = [
+  'Usage: theme [arg]',
+  'Args:',
+  ' - ls: list all available themes',
+  ' - set: set a theme',
+  '<br>',
+  'Example:',
+  ' theme ls # to list all available themes',
+  ' <span style="color:skyblue;">theme set light</span> # to set a theme',
+];
+
+
+
+
+help = [
+  '<span class="command">about  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">about me</span>',
+  '<span class="command">banner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span> <span class="helpdes">Display the header</span>',
+  '<span class="command">certificates &nbsp;&nbsp;- </span>  <span class="helpdes">   View IT Certifications</span>',
+  '<span class="command">clear | cls   &nbsp;&nbsp; - </span>  <span class="helpdes">   Clear the teminal</span>',
+  '<span class="command">date  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span> <span class="helpdes">    Display the actual date</span>',
+  '<span class="command">help  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span>     <span class="helpdes">Check available commands</span>',
+  '<span class="command">social &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span>  <span class="helpdes">   Display social networks</span>',
+  '<span class="command">theme    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> Theme configurations</span>',
+  '<span class="command">pwd  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;          - </span>    <span class="helpdes"> Print present working directory</span>',
+  '<br>',
+  '[TAB]              Trigger completion',
+  '[Ctrl+l]           Clear terminal',
+  '[Ctrl+p]           Go back to previous command',
+];
+
+ 
+banner = [
+'<span style="color: yellowgreen;">visitor@terminal.stefanmjoseph:~$</span> Welcome',
+' &nbsp;',
+' &nbsp;',
+' ',
+//"╔═╗═╦═ ╔═ ╔═  ╔  ╗",    _____
+//"╚═╗ ║  ╠═ ╠═   ",    
+//"╚═╝ ║  ╚═ ║    ",
+'&nbsp;',
+'&nbsp;',
+//" <span style='color:red;'>01101000 01101001</span> There 👋, I'm <span style='color: yellow;'>STEFAN JOSEPH</span> ",
+//'Welcome To my Terminal Portfolio !',
+' - - - -',
+'&nbsp;',
+
+'For a list of available commands, type<span class="command">\'help\'</span> ',
+
+];
+
