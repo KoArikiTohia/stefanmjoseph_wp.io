a3c1f013b24623bc80d83e52a5cc9644f9e5a790
Stefan Joseph <stefan.joseph@lethbridgecollege.ca>
2023-11-27 11:52:46 -0700

    Update commands.js
    Added new let commands
    Started with the certification, references, work and education code blocks
    Edited the whoami information

--- js/commands.js
+++ js/commands.js
@@ -1,42 +1,43 @@
 let linkedin = 'https://www.linkedin.com/in/stefanmjoseph/';
 let instagram ='https://www.instagram.com/stefanmjoseph/';
-let tryhackme= 'https://tryhackme.com/p/KoArikiTohia';
+let tryhackme = 'https://tryhackme.com/p/KoArikiTohia';
+
 let existCommands = [
   'banner',
-  'certifications',
+  'certs', // Add certs
   'clear',
-  'cls',
+  'cls', 
   'date',
-  'education',
+  'education', // add education 
   'help',
+  'instagram',
   'linkedin',
-  'projects',
-  'references',
+  'references', // add references
   'social',
   'theme',
   'theme ls',
   'theme set',
   'tryhackme',
   'whoami',
-  'workexp',
+  'workexp', // add work experience
   'cd ..'
 ];
 
 whoami = [
+    "&nbsp;",
+  "Hello my name is <span style='color: skyblue;'>Stefan Joseph</span>😊 ",
+    "&nbsp;",
+  "I'm a Applied Business Management Graduate from BYU-I. I have a passion ",
+  "for Cyber Security and working with people. I'm currently completing my",
+  "Computer Information Technology diploma at Lethbridge College",  
   "&nbsp;",
-  "<span style='color:red;'>01001000 01101001</span> I'm <span style='color: yellow;'>STEFAN JOSEPH </span> ",
-  "&nbsp;",
-  "I'm a BYU-I Business graduate who is passionate about Cyber Security and connecting",   
-  "with people. I'm currently finishing my CIT Diploma at Lethbridge College.",
-  "Problems are just Opportunities that haven't presented themselves - Wilson Fisk", 
-  "&nbsp;",
-  "&nbsp;",
-  'Connect With me 💫',
-'&nbsp;',
-// '<p align="center">',
-
-//'<a href="" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="sujay" height="30" width="40" /></a> <a href="https://wa.me/918762406259?text=01101000%2001101001%20%20%F0%9F%91%8B%2C%20SUJAY%20I%20got%20your%20number%20from%20your%20site%20.%20can%20we%20have%20a%20small%20conversation%20now%20%3F%20" target="blank"><img align="center" src="https://user-images.githubusercontent.com/95465072/199298950-3882322d-7527-47d0-ac6b-b81a0ed1a1aa.png" alt="sujay_adkesar" height="45" width="50" /></a>&nbsp;&nbsp;<a href="https://www.linkedin.com/in/sujay-adkesar-a90725224" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="utkar5hm" height="30" width="40" /></a> <a href="https://instagram.com/SUJAY_ADKESAR" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="sujay" height="30" width="40" /></a>',
-
+  "<span style='color: lightgreen;'>Mission Statement:</span> ",
+  "To develop integrity, to show a greater amount of empathy to all people I ",   
+  "influence and to strive for constant success even through setbacks.",
+    "&nbsp;",
+  "<span style='color: lightgreen;'>Problems are just Opportunities that haven't presented themselves - Wilson Fisk</span>", 
+ 
+  '<p align="center">',
 
 '&nbsp;',
 '&nbsp;',
@@ -52,7 +53,6 @@ social = [
   '<a href="https://tryhackme.com/p/KoArikiTohia/" style="color: skyblue;">TryHackMe</a>'
 ];
 
-
 themes = ['dark', 'light'];
 
 theme = [
@@ -66,45 +66,63 @@ theme = [
   ' <span style="color:skyblue;">theme set light</span> # to set a theme',
 ];
 
-
-
-
 help = [
-  '<span class="command">banner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span> <span class="helpdes">Display the header</span>',
-  '<span class="command">dertificates &nbsp;&nbsp;- </span>  <span class="helpdes">   View IT Certifications</span>',
-  '<span class="command">clear | cls   &nbsp;&nbsp; - </span>  <span class="helpdes">   Clear the teminal</span>',
-  '<span class="command">date  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span> <span class="helpdes">    Display the actual date</span>',
-  '<span class="command">help  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;         - </span>     <span class="helpdes">Check available commands</span>',
-  '<span class="command">project  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">Projects I have completed</span>',
-  '<span class="command">references  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">References from my curreny and previous employers</span>',
-  '<span class="command">resume  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">A resume containing all my work experience</span>',
-  '<span class="command">social &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - </span>  <span class="helpdes">   Display social networks</span>',
-  '<span class="command">theme    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;      - </span>    <span class="helpdes"> Theme configurations</span>',
-  '<span class="command">pwd  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;          - </span>    <span class="helpdes"> Print present working directory</span>',
-  '<span class="command">whoami  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;        - </span>     <span class="helpdes">About me</span>',
+  '<span class="command">banner &nbsp; ➡ </span> <span class="helpdes">Displays the header</span>',
+  '<span class="command">certs &nbsp; ➡ </span> <span class="helpdes">Displays my certifications in a new window</span>',
+  '<span class="command">clear &nbsp; ➡ </span> <span class="helpdes">Clears the Terminal</span>',
+  '<span class="command">date &nbsp; ➡ </span> <span class="helpdes">Displays the current date and time</span>',
+  '<span class="command">education &nbsp; ➡ </span> <span class="helpdes">Displays my education</span>',
+  '<span class="command">help &nbsp; ➡ </span> <span class="helpdes">Displays available commands</span>',
+  '<span class="command">references &nbsp; ➡ </span> <span class="helpdes">Displays work references</span>',
+  '<span class="command">social &nbsp; ➡ </span> <span class="helpdes">Displays my social profile links</span>',
+  '<span class="command">theme &nbsp; ➡ </span> <span class="helpdes">Theme Configuration</span>',
+  '<span class="command">pwd &nbsp; ➡ </span> <span class="helpdes">Prints the present working directory</span>',
+  '<span class="command">whoami &nbsp; ➡ </span> <span class="helpdes">Displays information about me</span>',
+  '<span class="command">workexp &nbsp; ➡ </span> <span class="helpdes">Displays my work experience</span>',
+ 
   '<br>',
   '[TAB]              Trigger completion',
   '[Ctrl+l]           Clear terminal',
   '[Ctrl+p]           Go back to previous command',
 ];
 
- 
 banner = [
-'<span style="color: yellowgreen;">visitor@terminal.stefanmjoseph:~$</span> Welcome',
+'<span style="color: yellowgreen;">visitor@terminal.stefanmjoseph:~$</span> Greetings 😊',
 ' &nbsp;',
-' &nbsp;',
-' ',
-"╔═╗═╦═ ╔═ ╔═  ╔  ╗",    _____
-//"╚═╗ ║  ╠═ ╠═   ",    
-//"╚═╝ ║  ╚═ ║    ",
+"╔═╗═╦═ ╔═ ╔═ ╔══╗ ╔═╗ 	═╦═ ╔ ╗ ╔═╗ ╔═ ╔═╗ ║ ║",
+"╚═╗ ║  ╠═ ╠═ ║══║ ║ ║   ║  ║ ║ ╚═╗ ╠═ ╠═╝ ╠═╣",
+"╚═╝ ║  ╚═ ║  ║  ║ ║ ║ ╚═╝  ╚ ╝ ╚═╝ ╚═ ║   ║ ║",
 '&nbsp;',
-'&nbsp;',
-//" <span style='color:red;'>01101000 01101001</span> There 👋, I'm <span style='color: yellow;'>STEFAN JOSEPH</span> ",
-//'Welcome To my Terminal Portfolio !',
-' - - - -',
+"Hello There!🤖, My name is <span style='color: skyblue;'>STEFAN JOSEPH</span> ",
+'&nbsp',
+'Welcome To My Web Portfolio!',
+//' - - - -',
 '&nbsp;',
 
 'For a list of available commands, type<span class="command">\'help\'</span> ',
 
 ];
 
+
+certs =[
+
+
+
+];
+
+education = [
+
+
+];
+
+references = [
+
+
+];
+
+workexp = [
+
+
+
+
+];
